81ecd556066f4a79a1abf661174f8b0b6cd845b6
mira <mkyshchak@gmail.com>
2025-03-17 16:10:41 +0200

    'donio'

--- task.sql
+++ task.sql
@@ -1,1 +1,12 @@
--- write your code for database user creation here
+
+USE ShopDB;
+
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+FLUSH PRIVILEGES;
